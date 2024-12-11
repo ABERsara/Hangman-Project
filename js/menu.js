c021ab0cbf7d8eb93633020561cceb55622623be
ABERsara <sara0556799675@gmail.com>
2024-12-11 16:03:31 +0200

    first commit

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const home=document.querySelector(".home")
+const about=document.querySelector(".about")
+const enter=document.querySelector(".enter")
+const regist=document.querySelector(".regist")
+
+home.addEventListener("click",()=>{
+    window.location.href ="./home.html" ;
+})
+
+enter.addEventListener("click",()=>{
+    window.location.href ="./enter.html" ;
+})
+
+regist.addEventListener("click",()=>{
+    window.location.href ="./Registration.html" ;
+})
+
